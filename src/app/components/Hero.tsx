d1eb9284a21683a8b826ffc1101ef6316833de42
Mazz-Ather <159653279+Mazz-Ather@users.noreply.github.com>
2024-12-11 10:07:37 +0500

    remove bottom sidebar

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -19,9 +19,10 @@ export default function Slide1(){
 
             className="xxs:m-3 z-10  xs:text-5xl xs:font-semibold  xs:m-3 xs:w-47 xl:w-[50vw] lg:!text-7xl  lg:mt-44 sm:!text-5xl md:text-6xl lg:w-[60vw] lg:px-5 md:w-[85%]  lg:ml-16 md:mt-24 md:ml-11 sm:w-[75%] sm:mt-20 sm:ml-20  xs:mt-20 xl:text-8xl   xl:font-bold xl:mt-36 xl:-ml-8 xl:m-5 "
           />
+          
            <p className="xxs:text-[19px] xxs:m-3 xxs:font-semibold md:w-[85%] xs:text-[20px] xs:m-3 xs:font-semibold text-white/80  sm:text-[22px] text-lg mt-9 xs:w-47 text-center md:text-[18px] md:ml-11  lg:text-[21px] lg:font-medium lg:text-center lg:w-[80%] lg:ml-20 xl:-ml-6 "> My passion for coding is fueled by a desire to create <br />meaningful and impactful digital solutions, constantly pushing <br /> the boundaries of innovation and user experience!</p>
              <Link
-              href="/mycv.pdf"
+              href="/mycv.pdf" 
               target="_blank"
               className=""
               download={true}
